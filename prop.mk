f8559f9156b7198dea8906f996c80427bdb55ce7
sooti <sooti85@gmail.com>
2019-06-25 11:30:27 +0200

    tissot: add instagram to aux whitelist Front facing camera doesn't work if this isn't in
    Change-Id: I80c811e87cf3d11bde178437814ca186442bfde9

--- prop.mk
+++ prop.mk
@@ -7,7 +7,7 @@ ro.vendor.audio.sdk.fluencetype=fluence
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
-vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap \
+vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap,com.instagram.android \
 persist.camera.dual.camera=0
 
 # Dalvik
